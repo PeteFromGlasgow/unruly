a26d5379711838b774a9e7163fe1a7aed44d5ee7
Peter Kinnaird <peter@peterkinnaird.eu>
2013-05-13 09:16:37 -0400

    Add support for escaped strings.

--- Unruly.js
+++ Unruly.js
@@ -4,8 +4,6 @@
  * Find potential CSS rules in source code and compare them to rules in a
  * CSS or LESS source file to find potentially unused rules
  * 
- * Version 0.1.1
- * 
  * Usage:
  * 	node Unruly -c [css/less files] -s [source files]
  * 
@@ -17,8 +15,8 @@
 
  
  //Initialize default variables and regexes for matching
- var sourceClassRegex = new RegExp("class[ ]*=[ ]*[\"'][A-Za-z0-9 _-]*['\"]","igm");
- var sourceIdRegex = new RegExp("id[ ]*=[ ]*[\"'][A-Za-z0-9_-]*['\"]","igm");
+ var sourceClassRegex = new RegExp("class[ ]*=[\\\\?[ ]*[\"'][A-Za-z0-9 _-]*[\\\\]?['\"]","igm");
+ var sourceIdRegex = new RegExp("id[ ]*=[\\\\?[ ]*[\"'][A-Za-z0-9 _-]*[\\\\]?['\"]","igm");
  
  var cssClassRegex = new RegExp("\\.[A-Za-z0-9-_]+","igm");
  var cssIdRegex = new RegExp("#[A-Za-z0-9-_]+","igm");
@@ -75,14 +73,14 @@ function parseCss(filename){
 
 	if (classes){
 		for (var i = classes.length - 1; i >= 0; i--) {
-			cssRules[classes[i]] = 1;
+			cssRules[classes[i]] = filename;
 		};
 	}
 	if (ids){
 		for (var i = ids.length - 1; i >= 0; i--) {
 			//Attempt to ignore all colors.
 			if (ids[i].search(/#([a-f]|[A-F]|[0-9]){3}(([a-f]|[A-F]|[0-9]){3})?\b/) == -1){
-				cssRules[ids[i]] = 1;
+				cssRules[ids[i]] = filename;
 			}
 		};
 	}
@@ -103,7 +101,7 @@ function parseSrc(filename){
 			} else {
 				rule = "#"+ids[i].substring(ids[i].indexOf('"')+1,ids[i].length-1);
 			}
-			usedRules[rule] = 1;
+			usedRules[rule] = filename;
 		}
 	}
 
@@ -117,7 +115,7 @@ function parseSrc(filename){
 			}
 			var rules = rule.split(" ");
 			for (var j = 0; j < rules.length; j++) {
-			 	usedRules["."+rules[j]] = 1;
+			 	usedRules["."+rules[j]] = filename;
 			 };
 		}
 	}
